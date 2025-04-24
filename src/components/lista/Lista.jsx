868905066074d09291b0e8de8cf41b7a0d88cdce
Anna Clara <annaclaraaraujo229@gmail.com>
2025-04-24 16:51:28 -0300

    feat: Criando elementos da tabela listar

--- src/components/lista/Lista.jsx
+++ src/components/lista/Lista.jsx
@@ -1,8 +1,9 @@
 import "./Lista.css";
-
+import Editar from "../../assets/img/pen-to-square-solid.svg"
+import Excluir from "../../assets/img/trash-can-regular.svg"
 const Lista = () =>{
     return(
-        <section>
+        <section className="layout_grid listagem">
             <h1>Lista dos Filmes</h1>
             <hr/>
 
@@ -20,6 +21,13 @@ const Lista = () =>{
                 </thead>
                 <tbody>
                     {/* tbody =>corpo da tabela */}
+                    <tr className="item_lista">
+                        <td>Gente Grande</td>
+                        <td>Comedia</td>
+                        <td><img src={Editar} alt="Caneta"/></td>
+                        <td><img src={Excluir} alt="Lixeira"/></td> 
+                        
+                    </tr>
                 </tbody>
             </table>
         </div>
